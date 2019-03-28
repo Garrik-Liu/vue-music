a49acf25fc3bdcc5355a9ade20b703263b8030d5
Garrik-Liu <garrikliu@Gmail.com>
2019-03-28 23:04:25 +0800

    [add][package]:添加 "fastclikc", "vue-lazyload"

--- src/main.js
+++ src/main.js
@@ -1,18 +1,32 @@
+// 核心库
 import Vue from 'vue'
 import Router from 'vue-router'
 import App from './App.vue'
 
+// 自定义配置文件
 import routes from 'router';
 import 'common/style/index.scss';
 
-Vue.config.productionTip = false
+// 第三方库
+import VueLazyLoad from 'vue-lazyload';
+import fastclick from 'fastclick';
 
+// 配置参数设置
+Vue.config.productionTip = false;
+
+// 插件使用
 Vue.use(Router);
+Vue.use(VueLazyLoad, {
+    loading: require('common/images/loading.gif')
+})
+fastclick.attach(document.body);
 
+// 创建路由
 const router = new Router({
     routes
 })
 
+// 创建 Vue 根实例
 new Vue({
     render: h => h(App),
     router
